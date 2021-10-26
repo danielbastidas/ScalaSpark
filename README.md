bf785498ef590db61af0c88db7c8aaaa6e605085
danielbastidas <dabastidasg@gmail.com>
2021-10-26 20:55:49 +0200

    Persisting the factored dataset on HDFS

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ScalaSpark
+Execute the following command inside the project folder to compile and generate the jar file <br/>
+<code>gradle jar<code>
--- src/main/scala/com/myscala/spark/HdfsTest.scala
+++ src/main/scala/com/myscala/spark/HdfsTest.scala
@@ -1,6 +1,11 @@
 package com.myscala.spark
 
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.StringIndexer
+
+/* Compile and generate the jar using the following command: gradle jar
+* from build.gradle file.
+* The file build.sbt is not used */
 
 /** This class can be executed from spark using the following command:
  spark-submit --packages org.apache.spark:spark-avro_2.12:3.1.1 --class \
@@ -21,22 +26,38 @@ object HdfsTest {
   /** Usage: HdfsTest [file] */
   def main(args: Array[String]): Unit = {
     if (args.length < 1) {
-      System.err.println("Usage: HdfsTest <file>")
+      System.err.println("Usage: HdfsTest <file> <number_of_records_to_show>")
       System.exit(1)
     }
+
+    var numberRecordsShow = 10
+
+    if (!args(1).toInt.isNaN) {
+      numberRecordsShow = Integer.valueOf(args(1))
+    }
+
     val spark = SparkSession
       .builder
       .appName("HdfsTest")
       .getOrCreate()
 //    val schema = "id DOUBLE, female STRING, ses STRING, schtyp STRING, prog STRING, read DOUBLE, write DOUBLE, " +
 //      "math DOUBLE, science DOUBLE, socst DOUBLE, honors STRING, awards DOUBLE, cid INT"
-    val file = spark.read.format("avro")/*.schema(schema)*/.load(args(0))
-    println(s"!!!!!!!!!!!!!!Number of records: ${file.count()}")
+    val data = spark.read.format("avro")/*.schema(schema)*/.load(args(0))
+    println(s"!!!!!!!!!!!!!!Number of records: ${data.count()}")
 //    println(s"!!!!!!!!!!!!!!Here comes the count: ${file.select("name", "favorite_color").show(10)}")
-    println(s"!!!!!!!!!!!!!!Data: ${file.show(10)}")
+//    println(s"!!!!!!!!!!!!!!Data: ${data.show(numberRecordsShow)}")
+
+    val indexer = new StringIndexer()
+      .setInputCols( Array("female", "ses", "schtyp", "prog", "honors") )
+      .setOutputCols( Array("femaleFactored", "sesFactored", "schtypFactored", "progFactored", "honorsFactored"))
+
+    val factored = indexer.fit(data).transform(data)
+
+    println(s"!!!!!!!!!!!!!!Data2: ${factored.show(numberRecordsShow)}")
+
+    factored.write.format("avro").save("hdfs://localhost:9000/media/danielbastidas/HardDisk/BigData/hadoop/datanode" +
+      "/sink/parquet-factored")
 
     spark.stop()
   }
 }
-
-// TODO: write data in parquet format 
